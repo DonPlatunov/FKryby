d0041b46bcc7bd2e4067ec3a4d003aa194bb4eb6
BVNCH <151386694+BVNCH@users.noreply.github.com>
2026-03-01 22:59:07 +0100

    Add files via upload

--- ryby.sql
+++ ryby.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2deb2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sty 29, 2026 at 06:30 PM
--- Wersja serwera: 8.4.7-0ubuntu0.25.10.3
--- Wersja PHP: 8.4.11
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2026 at 10:58 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `ryby`
+-- Database: `ryby`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,40 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `przynety` (
-  `id` int NOT NULL,
-  `nazwa` varchar(128) COLLATE utf8mb4_polish_ci NOT NULL,
-  `opis` varchar(1024) COLLATE utf8mb4_polish_ci NOT NULL,
-  `zdjecie` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(128) NOT NULL,
+  `opis` varchar(1024) NOT NULL,
+  `zdjecie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `przynety`
+--
+
+INSERT INTO `przynety` (`id`, `nazwa`, `opis`, `zdjecie`) VALUES
+(1, 'Biały robak', 'Najbardziej uniwersalna przynęta wędkarska. Dzięki ruchowi i zapachowi skutecznie wabi płoć, leszcza i karasia. Może być stosowany pojedynczo lub w zestawie kilku sztuk.', 'Biały robak.jpg'),
+(2, 'Boilies', 'Kulki proteinowe stosowane głównie przy połowie dużych karpi i amurów. Bardzo selektywne.', 'Boilies.jpg'),
+(3, 'Błystka obrotowa', 'Metalowa przynęta generująca drgania i błysk. Skuteczna na okonia i klenia.', 'Błystka obrotowa.jpg'),
+(4, 'Błystka wahadłowa', 'Cięższa błystka do połowu większych drapieżników, takich jak szczupak i sandacz.', 'Błystka wahadłowa.jpg'),
+(5, 'Chleb', 'Naturalna przynęta unosząca się w toni lub przy powierzchni. Szczególnie skuteczna na wzdręgi i karasie.', 'Chleb.jpg'),
+(6, 'Ciasto wędkarskie', 'Miękka przynęta o różnych aromatach, idealna na karasie i płocie. Dobrze pracuje na spławiku.', 'Ciasto wędkarskie.jpg'),
+(7, 'Czerwony robak', 'Przynęta o intensywnym kolorze i zapachu, szczególnie skuteczna na leszcze, liny i karasie. Działa dobrze na dnie i w wolno płynącej wodzie.', 'Czerwony robak.jpg'),
+(8, 'Filet rybny', 'Naturalna, intensywnie pachnąca przynęta na sandacza i suma. Bardzo skuteczna przy połowie gruntowej.', 'Filet rybny.jpg'),
+(9, 'Gumy (twistery, rippery)', 'Sztuczne przynęty imitujące rybki lub larwy. Bardzo skuteczne na okonia i sandacza.', 'Gumy (twistery, rippery).jpg'),
+(10, 'Kukurydza', 'Słodka i dobrze widoczna przynęta, bardzo odporna na drobnicę. Najczęściej stosowana na karpia, amura i większe leszcze.', 'Kukurydza.jpg'),
+(11, 'Martwa rybka', 'Naturalna przynęta drapieżna, używana do połowu sandacza i szczupaka. Działa dzięki zapachowi i wyglądowi.', 'Martwa rybka.jpg'),
+(12, 'Ochotka', 'Delikatna, naturalna przynęta używana głównie do połowu płoci i leszczy. Sprawdza się w trudnych warunkach i przy słabym żerowaniu ryb.', 'Ochotka.jpg'),
+(13, 'Pellet', 'Twarda, selektywna przynęta o wysokiej wartości odżywczej. Skuteczna na karpie i leszcze.', 'Pellet.jpg'),
+(14, 'Rosówka', 'Duży robak o silnym zapachu, skuteczny na liny, leszcze oraz drapieżniki, takie jak sandacz.', 'Rosówka.jpg'),
+(15, 'Wobler', 'Twarda przynęta imitująca żywą rybkę. Stosowana na szczupaka i bolenia.', 'Wobler.jpg'),
+(16, 'Sucha mucha', 'Sztuczna mucha imitująca owada na powierzchni wody', 'sucha_mucha.jpg'),
+(17, 'Mokra mucha', 'Mucha pracująca pod powierzchnią wody', 'mokra_mucha.jpg'),
+(18, 'Nimfa', 'Imitacja larwy owada wodnego', 'nimfa.jpg'),
+(19, 'Małe nimfy', 'Drobne nimfy stosowane w metodzie muchowej', 'male_nimfy.jpg'),
+(20, 'Streamer', 'Sztuczna przynęta imitująca małą rybkę', 'streamer.jpg'),
+(21, 'Spinnerbait', 'Obrotowa przynęta drapieżna', 'spinnerbait.jpg'),
+(22, 'Plankton', 'Przynęta stosowana przy połowie tołpygi', 'plankton.jpg'),
+(23, 'Drobne mięso rybne', 'Małe kawałki ryby', 'mieso_rybne.jpg');
 
 -- --------------------------------------------------------
 
@@ -41,14 +70,14 @@ CREATE TABLE `przynety` (
 --
 
 CREATE TABLE `rybki` (
-  `id` int NOT NULL,
-  `nazwa` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL,
-  `opis` text COLLATE utf8mb4_polish_ci NOT NULL,
-  `zdjecie` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(255) NOT NULL,
+  `opis` text NOT NULL,
+  `zdjecie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
--- Zrzut danych tabeli `rybki`
+-- Dumping data for table `rybki`
 --
 
 INSERT INTO `rybki` (`id`, `nazwa`, `opis`, `zdjecie`) VALUES
@@ -82,10 +111,72 @@ INSERT INTO `rybki` (`id`, `nazwa`, `opis`, `zdjecie`) VALUES
 --
 
 CREATE TABLE `ryby_przynety` (
-  `id` int NOT NULL,
-  `rybka_id` int NOT NULL,
-  `przyneta_id` int NOT NULL
+  `id` int(11) NOT NULL,
+  `rybka_id` int(11) NOT NULL,
+  `przyneta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `ryby_przynety`
+--
+
+INSERT INTO `ryby_przynety` (`id`, `rybka_id`, `przyneta_id`) VALUES
+(1, 1, 14),
+(2, 1, 8),
+(3, 2, 15),
+(4, 2, 4),
+(5, 2, 11),
+(6, 3, 12),
+(7, 3, 7),
+(8, 4, 7),
+(9, 4, 1),
+(10, 4, 14),
+(11, 5, 13),
+(12, 6, 12),
+(13, 6, 1),
+(14, 7, 14),
+(15, 7, 8),
+(16, 7, 7),
+(17, 8, 5),
+(18, 8, 6),
+(19, 9, 10),
+(20, 9, 13),
+(21, 9, 6),
+(22, 10, 3),
+(23, 10, 15),
+(24, 11, 7),
+(25, 11, 1),
+(26, 11, 14),
+(27, 12, 1),
+(28, 12, 12),
+(29, 12, 14),
+(30, 13, 3),
+(31, 14, 1),
+(32, 14, 10),
+(33, 14, 6),
+(34, 15, 10),
+(35, 15, 6),
+(36, 15, 1),
+(37, 16, 12),
+(38, 16, 1),
+(39, 16, 5),
+(40, 17, 10),
+(41, 17, 13),
+(42, 17, 2),
+(43, 18, 10),
+(44, 18, 13),
+(45, 18, 2),
+(46, 19, 7),
+(47, 19, 12),
+(48, 19, 1),
+(49, 20, 7),
+(50, 20, 5),
+(51, 20, 3),
+(52, 21, 7),
+(53, 21, 14),
+(54, 21, 12),
+(55, 22, 12),
+(56, 22, 1);
 
 -- --------------------------------------------------------
 
@@ -94,9 +185,9 @@ CREATE TABLE `ryby_przynety` (
 --
 
 CREATE TABLE `ryby_zanety` (
-  `id` int NOT NULL,
-  `rybka_id` int NOT NULL,
-  `zaneta_id` int NOT NULL
+  `id` int(11) NOT NULL,
+  `rybka_id` int(11) NOT NULL,
+  `zaneta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 -- --------------------------------------------------------
@@ -106,10 +197,10 @@ CREATE TABLE `ryby_zanety` (
 --
 
 CREATE TABLE `zanety` (
-  `id` int NOT NULL,
-  `nazwa` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL,
-  `opis` text COLLATE utf8mb4_polish_ci NOT NULL,
-  `zdjecie` varchar(255) COLLATE utf8mb4_polish_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(255) NOT NULL,
+  `opis` text NOT NULL,
+  `zdjecie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
@@ -147,38 +238,38 @@ ALTER TABLE `zanety`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `przynety`
+-- AUTO_INCREMENT for table `przynety`
 --
 ALTER TABLE `przynety`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT dla tabeli `rybki`
+-- AUTO_INCREMENT for table `rybki`
 --
 ALTER TABLE `rybki`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT dla tabeli `ryby_przynety`
+-- AUTO_INCREMENT for table `ryby_przynety`
 --
 ALTER TABLE `ryby_przynety`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
--- AUTO_INCREMENT dla tabeli `ryby_zanety`
+-- AUTO_INCREMENT for table `ryby_zanety`
 --
 ALTER TABLE `ryby_zanety`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT dla tabeli `zanety`
+-- AUTO_INCREMENT for table `zanety`
 --
 ALTER TABLE `zanety`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
